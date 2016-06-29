eeccc7df2758444ec056e5c415e4b99db2d2e563
xumingjun <xumingjun@example.com>
2016-06-29 15:31:21 +0800

    引导面-手机动画效果实现

--- app/src/main/java/gitdroid/zhuoxin/com/gitdroid/splash/SplashPagerFragment.java
+++ app/src/main/java/gitdroid/zhuoxin/com/gitdroid/splash/SplashPagerFragment.java
@@ -9,6 +9,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
 
 import butterknife.Bind;
 import butterknife.BindColor;
@@ -24,7 +25,10 @@ public class SplashPagerFragment extends Fragment {
     @Bind(R.id.viewPager)ViewPager viewPager;
     @Bind(R.id.indicator)CircleIndicator indicator;//指示器
     @Bind(R.id.content)FrameLayout frameLayout;
-    @Bind(R.id.layoutPhone)FrameLayout layoutPhone;
+    @Bind(R.id.layoutPhone)FrameLayout layoutPhone;//手机layout
+    @Bind(R.id.ivPhoneBlank)ImageView ivPhoneBlank;
+    @Bind(R.id.ivPhone)ImageView ivPhone;
+
     @BindColor(R.color.colorGreen) int colorGreen;   // ViewPager页面对应背景色
     @BindColor(R.color.colorRed) int colorRed;  // ViewPager页面对应背景色
     @BindColor(R.color.colorYellow) int colorYellow;    // ViewPager页面对应背景色
@@ -44,6 +48,7 @@ public class SplashPagerFragment extends Fragment {
         viewPager.setAdapter(adapter);
         // 添加ViewPager监听
         viewPager.addOnPageChangeListener(pageChangeListener);
+
         indicator.setViewPager(viewPager);
     }
     private final ViewPager.OnPageChangeListener pageChangeListener = new ViewPager.OnPageChangeListener(){
@@ -55,20 +60,28 @@ public class SplashPagerFragment extends Fragment {
             //start颜色：做一个
             //end颜色：...
             if (position==0){
-                layoutPhone.setVisibility(View.GONE);
+                //移动过程中有一个平移动画
+                int scroll =(int)((positionOffset-1) * 150);
+                //移动过程中的缩放
+                layoutPhone.setTranslationX(scroll);
+                float scale = 0.3f + positionOffset * 0.7f ;
+                layoutPhone.setScaleX(scale);
+                layoutPhone.setScaleY(scale);
+                //移动过程中，手机内容的显示
+                ivPhone.setAlpha(positionOffset);
                 int color = (int)evaluator.evaluate(positionOffset,colorGreen,colorRed);
                 frameLayout.setBackgroundColor(color);
                 return;
             }
+            //当在2页面与3页面之间
             if (position==1){
-                layoutPhone.setVisibility(View.VISIBLE);
+
+                layoutPhone.setTranslationX(-positionOffsetPixels);
                 int color = (int)evaluator.evaluate(positionOffset,colorRed,colorYellow);
                 frameLayout.setBackgroundColor(color);
                 return;
             }
-            if (position==2){
-                layoutPhone.setVisibility(View.GONE);
-            }
+
 
 
         }
@@ -76,13 +89,13 @@ public class SplashPagerFragment extends Fragment {
         @Override
         public void onPageSelected(int position) {
             if (position==0){
-                layoutPhone.setVisibility(View.GONE);
+                layoutPhone.setVisibility(View.VISIBLE);
             }
             if (position==1){
                 layoutPhone.setVisibility(View.VISIBLE);
             }
             if (position==2){
-                layoutPhone.setVisibility(View.GONE);
+
                 Pager2 pager2 = (Pager2) adapter.getViews(2);
                 pager2.showAnimation();
             }

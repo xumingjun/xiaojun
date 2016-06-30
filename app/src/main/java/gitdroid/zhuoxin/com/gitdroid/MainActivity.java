71b83810eda7f6311e390141367a17298ebc79e6
xumingjun <xumingjun@example.com>
2016-06-30 10:21:30 +0800

    ActionBarDrawerToggle实现点击开启侧滑，并带有动画。

--- app/src/main/java/gitdroid/zhuoxin/com/gitdroid/MainActivity.java
+++ app/src/main/java/gitdroid/zhuoxin/com/gitdroid/MainActivity.java
@@ -3,8 +3,10 @@ package gitdroid.zhuoxin.com.gitdroid;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.widget.GridLayout;
 
@@ -19,7 +21,9 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     @Bind(R.id.navigationView)NavigationView navigationView;//侧滑菜单试图
     @Bind(R.id.drawerLayout)DrawerLayout drawerLayout;
+    @Bind(R.id.toolbar)Toolbar toolbar;
     private ActivityUtils activityUtils;
+    private MenuItem menuItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,11 +38,33 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         ButterKnife.bind(this);
         navigationView.setNavigationItemSelectedListener(this);
         activityUtils = new ActivityUtils(this);
+        //处理
+        setSupportActionBar(toolbar);
+        //返回键显示
+//      getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //抽屉开关
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this,
+                drawerLayout,
+                toolbar,
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close
+        );
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();//同步状态
+
+        //默认选中最热门
+        menuItem = navigationView.getMenu().findItem(R.id.github_hot_repo);
+        menuItem.setChecked(true);
     }
 
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-
+        //将默认选中项手动设置为false
+        if (menuItem.isChecked()){
+            menuItem.setChecked(false);
+        }
         switch (item.getItemId()){
             case R.id.github_hot_repo:
                 activityUtils.showToast(R.string.hot_repo);
@@ -47,7 +73,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 activityUtils.showToast(R.string.my_repo);
                 break;
             case R.id.tips_daily:
-                activityUtils.showToast(R.id.tips_daily);
+                activityUtils.showToast(R.string.tips_daily);
                 break;
             /*case R.id.tips_share:
                 //测试代码，按下分享，关闭侧滑
